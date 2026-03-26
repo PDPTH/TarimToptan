18dfab10eab1459a9297def5ef5f8b68fe7d9593
Süleyman Buğra Çetin <11cetinsuleyman@gmail.com>
2026-03-26 18:18:05 +0300

    Update api.js

--- Web-Frontend/src/services/api/api.js
+++ Web-Frontend/src/services/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://api.yazmuh.com/v1',
+    baseURL: import.meta.env.VITE_API_URL || 'https://tarimtoptan.onrender.com',
     timeout: 30000,
     headers: {
         'Content-Type': 'application/json',
