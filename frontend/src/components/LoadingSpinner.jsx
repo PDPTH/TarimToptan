127f49ff1023581ba1fe5d183dab87ebe809aeb4
Süleyman Buğra Çetin <11cetinsuleyman@gmail.com>
2026-04-04 22:54:17 +0300

    Delete frontend directory

--- frontend/src/components/LoadingSpinner.jsx
+++
@@ -1,8 +0,0 @@
-export default function LoadingSpinner({ text = 'Yükleniyor...' }) {
-    return (
-        <div className="loading-container">
-            <div className="spinner" />
-            <p className="loading-text">{text}</p>
-        </div>
-    )
-}
